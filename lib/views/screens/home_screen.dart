2760b8a2658baf7a9f9795de2d17bdd250a8d8e8
reliek21 <keilerguardo@gmail.com>
2023-07-13 13:50:33 -0500

    feat: home screen finish

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:epicticker/common/color.dart';
+import 'package:epicticker/views/widgets/day_left_card_widget.dart';
+import 'package:epicticker/views/widgets/information_dash_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  Widget _body(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            margin: const EdgeInsets.symmetric(vertical: 46.0),
+            child: const InformationDashWidget()
+          ),
+          const DayLeftCardWidget(),
+          const DayLeftCardWidget(),
+          const DayLeftCardWidget(),
+          const DayLeftCardWidget(),
+          const DayLeftCardWidget(),
+          const DayLeftCardWidget(),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: EpicTickerColors.primary,
+      body: SafeArea(child: SingleChildScrollView(child: _body(context))),
+    );
   }
 }
