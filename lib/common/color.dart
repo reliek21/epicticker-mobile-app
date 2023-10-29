cf3d60430c218cf30baa891b9f501e7802d0dd65
reliek21 <keilerguardo@gmail.com>
2023-10-29 00:19:22 -0500

    feat: colors added

--- lib/common/color.dart
+++ lib/common/color.dart
@@ -7,4 +7,6 @@ class EpicTickerColors {
   static const Color secondary = Color(0xff00ADB5);
   static const Color accent = Color(0xffEEEEEE);
   static const Color accentBlack = Color(0xff393E46);
+  static const Color intensePink = Color(0xffD9376E);
+  static const Color green = Color(0xff2cb67d);
 }
