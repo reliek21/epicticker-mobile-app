318d8fa133299174e11a3a45adc06bf2476ed0d3
reliek21 <keilerguardo@gmail.com>
2023-12-10 02:49:48 -0500

    feat: secondary color changed

--- lib/common/color.dart
+++ lib/common/color.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 abstract class EpicTrackerColors {
   static const Color main = Color(0xff222831);
-  static const Color secondary = Color(0xffE16162);
+  static const Color secondary = Color(0xff994FF3);
   static const Color accent = Color(0xffEEEEEE);
   static const Color accentBlack = Color(0xff393E46);
   static const Color intensePink = Color(0xffD9376E);
